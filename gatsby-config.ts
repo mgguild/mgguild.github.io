04e65c4a96bfbe97671c0d86903eba76f32c9131
kojiadrianojr <iisparkplugiiog@gmail.com>
2022-04-26 11:43:21 +0800

    Setup google analytics

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -5,7 +5,24 @@ const config: GatsbyConfig = {
     title: `mgg-website`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-react-helmet"]
+  plugins: [ {
+    resolve: `gatsby-plugin-google-gtag`,
+    options: {
+      // You can add multiple tracking ids and a pageview event will be fired for all of them.
+      trackingIds: [
+        "G-N3FSWCLQTB", // Google Analytics / GA
+      ],
+      // This object gets passed directly to the gtag config command
+      // This config will be shared across all trackingIds
+
+      // This object is used for configuration specific to this plugin
+      pluginConfig: {
+        // Puts tracking script in the head instead of the body
+        head: true,
+        // Setting this parameter is also optional
+      },
+    },
+  }, "gatsby-plugin-styled-components", "gatsby-plugin-react-helmet"]
 };
 
 export default config;
