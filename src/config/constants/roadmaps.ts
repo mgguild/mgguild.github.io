449ebdddd1dd20a242d6121f4d8c6aa3a4c5949a
Jambs <salvadorjr.amba@gmail.com>
2022-05-25 16:16:11 +0800

    roadmap update & finish

---
+++ src/config/constants/roadmaps.ts
@@ -0,0 +1,63 @@
+import { RoadmapConfig } from "./types"
+
+const roadmaps: RoadmapConfig = {
+  2021 : {
+    Q3 : [
+      "Creation of MGG core team",
+      "Brainstorming, research and development",
+      "MGG logo and gameplay design",
+      "Generation of MGG funding and asset building",
+      "MGG content creation",
+      "MGG website development",
+      "Official launch of MGG (Website, social media accounts, email, etc)"
+    ],
+    Q4 : [
+      "TGE Launch and distribution of MGG tokens",
+      "Launch of MGG yeild Farming/Staking",
+      "Official Listing at Exchanges (DEX and CEX)",
+      "MGG Scholarship (1,000 to 1,500 scholars)",
+      "Team Expansion"
+    ]
+  },
+  2022 : {
+    Q1 : [
+      "TGE launch and Distribution of MGG Tokens",
+      "LP Farm and Single Asset Staking",
+      "IGO and INO Launchpad",
+      "MGG Gaming Scholarship Program (1,000 to 1,500 scholars)",
+      "Partnerships and Official Listing at Exchanges",
+      "1st buyback and burn program"
+    ],
+    Q2 : [
+      "GameFi Vault Official Launch",
+      "MGG PFP NFTs Launch",
+      "MGG NFT Game Production",
+      "PVE Mode and Combat Mechanics",
+      "Implement Levels 1 to 4",
+      "Onboarding of more scholars (1,500 to 5,000)",
+      "Updated MGG Website: UI/UX Refresh",
+      "Exchange Listing and Partnerships",
+      "2nd buyback and burn program"
+    ],
+    Q3 : [
+      "Earning Dashboard Launch",
+      "NFT Game Blockchain Integration",
+      "Implement Levels 5 to 9",
+      "Exchange Listing and Partnerships",
+      "ChainPort Bridge Integration",
+      "Continue onboarding of scholars (5,000 to 7,500)",
+      "Team Expansion",
+      "3rd buyback and burn program"
+    ],
+    Q4 : [
+      "NFT Game: Alpha version release",
+      "Launch of NFT Game Marketplace",
+      "Exchange Listing and Partnerships",
+      "Team Expansion",
+      "Continue onboarding of scholars (7,500 to 10,000)",
+      "4tth buyback and burn program"
+    ]
+  }
+}
+
+export default roadmaps
--- src/pages/Roadmap/roadmap.tsx
+++ src/pages/Roadmap/roadmap.tsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useState, useContext, useCallback } from 'react'
 import { Text, Flex, Heading } from '@metagg/mgg-uikit'
-import styled from 'styled-components'
+import styled, { ThemeContext } from 'styled-components'
+import { NavOption } from '../../style/Global'
+import roadmaps from '../../config/constants/roadmaps'
 
 const Card = styled.div`
   flex: 1;
@@ -13,7 +15,7 @@ const Card = styled.div`
   -moz-box-shadow: inset 0 0 2rem #000000;
   -webkit-box-shadow: inset 0 0 2rem #000000;
   box-shadow: inset 0 0 2rem #000000, 0 0 0.5rem #00f4fd;
-  
+  z-index: 3;
 `
 
 const RdmapList = styled.ul`
@@ -53,7 +55,7 @@ const Page = styled.div`
     background-image: url("/BG2.png");
     background-repeat: no-repeat;
     background-position: center;
-    background-size: auto;
+    background-size: cover;
   }
 `
 
@@ -62,10 +64,9 @@ const RdmapCards = styled(Flex)`
   flex-flow: row wrap;
   column-gap: 1rem;
   row-gap: 3rem;
-  justify-content: space-between;
+  justify-content: center;
   align-items: baseline;
   position: relative;
-  z-index: 4;
 
   @media (max-width: 1280px) {
     flex-flow: column wrap;
@@ -79,84 +80,107 @@ const HeadingGlow = styled(Heading)<{ glow?: string }>`
 `
 
 const Line = styled.div`
-  width: 100%;
-  background-color: rgb(253, 218, 0);
-  z-index: 3;
+  width: 10%;
+  background-color: rgb(253,218,0);
+  z-index: 0;
   position: absolute;
-  top: 10rem;
+  top: 5rem;
+  left: 100%;
   border: solid #fdda00;
   box-shadow: 0 0 1rem #fdda00;
 
   @media (max-width: 1280px) {
     width: 0.1rem;
     left: 50%;
-    height: 80%;
+    top: 100%;
+    height: 15%;
   }
 `
 
+// const latest = roadmaps[Object.keys(roadmaps).pop()];
+// console.log(latest)
 
 const Roadmap = () => {
+  const theme = useContext(ThemeContext)
+  const [active, setActive] = useState(Object.keys(roadmaps).length)
+  const [getYear, setYear] = useState(roadmaps[Object.keys(roadmaps).pop()])
+
   return (
     <>
       <Page>
         <div style={{position: 'relative', zIndex: 2}}>
           <HeadingGlow size='xxl' color='#fdda00' glow="#fdda00">Roadmap</HeadingGlow>
-          <Line />
+          <Flex
+             alignItems="center"
+             margin="10px 0px 20px 0px"
+             padding="0 0 1rem 0"
+             style={{
+              justifyContent: 'center',
+              borderBottom: `0.5px solid #00f4fd`,
+              width: '100%',
+              flexFlow: 'wrap row',
+              rowGap: '1rem',
+              columnGap: '1.5rem'
+            }}
+          >
+            <NavOption
+                key='all'
+                style={{borderRadius: '0.5rem', padding: '0 2rem'}}
+                onClick={() => {setActive(0)}}
+                activeIndex={active === 0}
+              >
+               All
+             </NavOption>
+            {Object.keys(roadmaps).map((year, index) => (
+              <NavOption
+                key={year}
+                style={{borderRadius: '0.5rem', padding: '0 2rem'}}
+                onClick={() => {setActive(index + 1); setYear(roadmaps[year])}}
+                activeIndex={active === index + 1}
+              >
+               {year}
+             </NavOption>
+            ))}
+          </Flex>
 
-          <RdmapCards>
-            <Card>
-              <HeadingGlow size='xl' color='#00f4fd'>Q1</HeadingGlow>
-              <RdmapList>
-                <li>TGE launch and Distribution of MGG Tokens</li>
-                <li>LP Farm and Single Asset Staking</li>
-                <li>IGO and INO Launchpad</li>
-                <li>MGG Gaming Scholarship Program (1,000 to 1,500 scholars)</li>
-                <li>Partnerships and Official Listing at Exchanges</li>
-                <li>1st buyback and burn program</li>
-              </RdmapList>
-            </Card>
 
-            <Card>
-              <HeadingGlow size='xl' color='#00f4fd'>Q2</HeadingGlow>
-              <RdmapList>
-                <li>GameFi Vault Official Launch</li>
-                <li>MGG PFP NFTs Launch</li>
-                <li>MGG NFT Game Production</li>
-                <li>PVE Mode and Combat Mechanics</li>
-                <li>Implement Levels 1 to 4</li>
-                <li>Onboarding of more scholars ( 1,500 to 5,000 )</li>
-                <li>Updated MGG Website: UI/UX Refresh</li>
-                <li>Exchange Listing and Partnerships</li>
-                <li>2nd buyback and burn program</li>
-              </RdmapList>
-            </Card>
+          {active !== 0 ?
+            <RdmapCards>
+              {Object.keys(getYear).map((quarter) => (
+                <div style={{position: 'relative'}}>
+                  {quarter !== 'Q4' && <Line />}
+                  <Card>
+                    <HeadingGlow size='xl' color='#00f4fd'>{quarter}</HeadingGlow>
+                    <RdmapList>
+                      {getYear[quarter].map((list) => <li>{list}</li>)}
+                    </RdmapList>
+                  </Card>
+                </div>
+              ))}
+            </RdmapCards>
+            :
+            <div>
+              {Object.keys(roadmaps).map((year) => (
+                <div style={{margin: '0 0 2rem 0'}}>
+                  <Heading size='xl'>{year}</Heading>
+                  <RdmapCards>
+                    {Object.keys(roadmaps[year]).map((quarter) => (
+                      <div style={{position: 'relative'}}>
+                        {quarter !== 'Q4' && <Line />}
+                        <Card>
+                          <HeadingGlow size='xl' color='#00f4fd'>{quarter}</HeadingGlow>
+                          <RdmapList>
+                            {roadmaps[year][quarter].map((list) => <li>{list}</li>)}
+                          </RdmapList>
+                        </Card>
+                      </div>
+                    ))}
+                  </RdmapCards>
+                </div>
+              ))}
+            </div>
+          }
 
-            <Card>
-              <HeadingGlow size='xl' color='#00f4fd'>Q3</HeadingGlow>
-              <RdmapList>          
-                <li>Earning Dashboard Launch</li>
-                <li>NFT Game Blockchain Integration</li>
-                <li>Implement Levels 5 to 9</li>
-                <li>Exchange Listing and Partnerships </li>
-                <li>ChainPort Bridge Integration</li>
-                <li>Continue onboarding of scholars ( 5,000 to 7,500 )</li>
-                <li>Team Expansion</li>
-                <li>3rd buyback and burn program</li>
-              </RdmapList>
-            </Card>
-
-            <Card>
-              <HeadingGlow size='xl' color='#00f4fd'>Q4</HeadingGlow>
-              <RdmapList>          
-                <li>NFT Game: Alpha version release</li>
-                <li>Launch of NFT Game Marketplace</li>
-                <li>Exchange Listing and Partnerships</li>
-                <li>Team Expansion</li>
-                <li>Continue onboarding of scholars ( 7,500 to 10,000 )</li>
-                <li>4tth buyback and burn program</li>
-              </RdmapList>
-            </Card>
-          </RdmapCards>
         </div>
       </Page>
     </>
