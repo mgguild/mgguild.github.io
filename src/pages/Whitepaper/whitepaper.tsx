11efe837ba557a4b8373a9879fc99c50b9268e89
Jambs <salvadorjr.amba@gmail.com>
2022-05-31 18:22:46 +0800

    minor responsive fix

--- src/pages/Whitepaper/whitepaper.tsx
+++ src/pages/Whitepaper/whitepaper.tsx
@@ -19,10 +19,10 @@ import PDF from './whitepaper/MetaGaming-Guild-White-Paper-02082022.pdf'
 
 const MggToken = () => {
   const theme = useContext(ThemeContext)
-  const [numPages, setNumPages] = useState(null);
-  const [pageNumber, setPageNumber] = useState(1);
 
-
+  const IframeOnLoad = (e: any) => {
+    console.log(e)
+  }
 
   return (
     <>
@@ -31,15 +31,14 @@ const MggToken = () => {
         <BgContainer bgImage={bgImage} bgColor='#140937' size='cover' style={{alignItems: 'center', justifyContent: 'center'}}>
           <Section>
             <HeadingGlow size='xxl' color={theme.colors.primary} glow={theme.colors.primary}>White Paper</HeadingGlow>
-            <div style={{position: 'relative', zIndex: 1, width: '100%', maxWidth: '50rem', height: '801px'}}>
-              <iframe src={PDF} width="100%" height="801px" style={{position: 'relative', zIndex: 3}}/>
-              <img src={PDFImg} style={{position: 'absolute', top: 0, left: 0, zIndex: 1}}/>
+            <div style={{position: 'relative', zIndex: 1, width: '100%', maxWidth: '50rem'}}>
+              <iframe onLoad={IframeOnLoad} src={PDF} width="100%" style={{position: 'relative', zIndex: 3, height: '100vh', maxHeight: '800px'}}/>
+              {/* <img src={PDFImg} style={{position: 'absolute', top: 0, left: 0, zIndex: 1}}/> */}
 
+              <a href={PDF}>
+                <Btn style={{margin: '1rem 0 0 0 '}}>Download PDF</Btn>
+              </a>
             </div>
-
-            <a href={PDF}>
-              <Btn>Download PDF</Btn>
-            </a>
           </Section>
         </BgContainer>
 
