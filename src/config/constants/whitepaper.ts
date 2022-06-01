dbcd1241d971f998edfc32ce92f6245499a33b14
Jambs <salvadorjr.amba@gmail.com>
2022-06-01 13:23:26 +0800

    white paper move assets & img gallery assets

---
+++ src/config/constants/whitepaper.ts
@@ -0,0 +1,138 @@
+import Page1 from 'assets/whitepaper/imgs/1.png'
+import Page2 from 'assets/whitepaper/imgs/2.png'
+import Page3 from 'assets/whitepaper/imgs/3.png'
+import Page4 from 'assets/whitepaper/imgs/4.png'
+import Page5 from 'assets/whitepaper/imgs/5.png'
+import Page6 from 'assets/whitepaper/imgs/6.png'
+import Page7 from 'assets/whitepaper/imgs/7.png'
+import Page8 from 'assets/whitepaper/imgs/8.png'
+import Page9 from 'assets/whitepaper/imgs/9.png'
+import Page10 from 'assets/whitepaper/imgs/10.png'
+import Page11 from 'assets/whitepaper/imgs/11.png'
+import Page12 from 'assets/whitepaper/imgs/12.png'
+import Page13 from 'assets/whitepaper/imgs/13.png'
+import Page14 from 'assets/whitepaper/imgs/14.png'
+import Page15 from 'assets/whitepaper/imgs/15.png'
+import Page16 from 'assets/whitepaper/imgs/16.png'
+import Page17 from 'assets/whitepaper/imgs/17.png'
+import Page18 from 'assets/whitepaper/imgs/18.png'
+import Page19 from 'assets/whitepaper/imgs/19.png'
+import Page20 from 'assets/whitepaper/imgs/20.png'
+import Page21 from 'assets/whitepaper/imgs/21.png'
+import Page22 from 'assets/whitepaper/imgs/22.png'
+
+import Thumbnail1 from 'assets/whitepaper/imgs/thumbnails/1.png'
+import Thumbnail2 from 'assets/whitepaper/imgs/thumbnails/2.png'
+import Thumbnail3 from 'assets/whitepaper/imgs/thumbnails/3.png'
+import Thumbnail4 from 'assets/whitepaper/imgs/thumbnails/4.png'
+import Thumbnail5 from 'assets/whitepaper/imgs/thumbnails/5.png'
+import Thumbnail6 from 'assets/whitepaper/imgs/thumbnails/6.png'
+import Thumbnail7 from 'assets/whitepaper/imgs/thumbnails/7.png'
+import Thumbnail8 from 'assets/whitepaper/imgs/thumbnails/8.png'
+import Thumbnail9 from 'assets/whitepaper/imgs/thumbnails/9.png'
+import Thumbnail10 from 'assets/whitepaper/imgs/thumbnails/10.png'
+import Thumbnail11 from 'assets/whitepaper/imgs/thumbnails/11.png'
+import Thumbnail12 from 'assets/whitepaper/imgs/thumbnails/12.png'
+import Thumbnail13 from 'assets/whitepaper/imgs/thumbnails/13.png'
+import Thumbnail14 from 'assets/whitepaper/imgs/thumbnails/14.png'
+import Thumbnail15 from 'assets/whitepaper/imgs/thumbnails/15.png'
+import Thumbnail16 from 'assets/whitepaper/imgs/thumbnails/16.png'
+import Thumbnail17 from 'assets/whitepaper/imgs/thumbnails/17.png'
+import Thumbnail18 from 'assets/whitepaper/imgs/thumbnails/18.png'
+import Thumbnail19 from 'assets/whitepaper/imgs/thumbnails/19.png'
+import Thumbnail20 from 'assets/whitepaper/imgs/thumbnails/20.png'
+import Thumbnail21 from 'assets/whitepaper/imgs/thumbnails/21.png'
+import Thumbnail22 from 'assets/whitepaper/imgs/thumbnails/22.png'
+
+const whitepaper = [
+  {
+    original: Page1,
+    thumbnail: Thumbnail1
+  },
+  {
+    original: Page2,
+    thumbnail: Thumbnail2
+  },
+  {
+    original: Page3,
+    thumbnail: Thumbnail3
+  },
+  {
+    original: Page4,
+    thumbnail: Thumbnail4
+  },
+  {
+    original: Page5,
+    thumbnail: Thumbnail5
+  },
+  {
+    original: Page6,
+    thumbnail: Thumbnail6
+  },
+  {
+    original: Page7,
+    thumbnail: Thumbnail7
+  },
+  {
+    original: Page8,
+    thumbnail: Thumbnail8
+  },
+  {
+    original: Page9,
+    thumbnail: Thumbnail9
+  },
+  {
+    original: Page10,
+    thumbnail: Thumbnail10
+  },
+  {
+    original: Page11,
+    thumbnail: Thumbnail11
+  },
+  {
+    original: Page12,
+    thumbnail: Thumbnail12
+  },
+  {
+    original: Page13,
+    thumbnail: Thumbnail13
+  },
+  {
+    original: Page14,
+    thumbnail: Thumbnail14
+  },
+  {
+    original: Page15,
+    thumbnail: Thumbnail15
+  },
+  {
+    original: Page16,
+    thumbnail: Thumbnail16
+  },
+  {
+    original: Page17,
+    thumbnail: Thumbnail17
+  },
+  {
+    original: Page18,
+    thumbnail: Thumbnail18
+  },
+  {
+    original: Page19,
+    thumbnail: Thumbnail19
+  },
+  {
+    original: Page20,
+    thumbnail: Thumbnail20
+  },
+  {
+    original: Page21,
+    thumbnail: Thumbnail21
+  },
+  {
+    original: Page22,
+    thumbnail: Thumbnail22
+  }
+]
+
+export default whitepaper
