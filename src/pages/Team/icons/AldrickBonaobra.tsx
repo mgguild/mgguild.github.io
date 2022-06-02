3a15d02d86b92ac344d5dcf23a2957dfe697c359
kojiadrianojr <iisparkplugiiog@gmail.com>
2022-06-02 15:59:50 +0800

    Load assets into teams page

---
+++ src/pages/Team/icons/AldrickBonaobra.tsx
@@ -0,0 +1,11 @@
+import React, { SVGAttributes, useContext } from 'react'
+import { ThemeContext } from 'styled-components'
+import PNG from 'assets/teams/5 T&D Aldrick.png'
+import { SvgProps } from '../../../components/svgIcon/types'
+import SvgIcon from '../../../components/svgIcon/SvgIcon'
+
+const Icon: React.FC<SvgProps> = (props) => {
+  return <SvgIcon Img={PNG} width={props.width} />
+}
+
+export default Icon
