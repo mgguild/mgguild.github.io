09c552715f0896b8cf3778b21a96f45bb9d767b0
Medard Mandane <medardm13@gmail.com>
2022-08-23 14:26:52 +0800

    Update mainnet address and chain ID

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,5 @@
 export const TESTNET_CHAIN_ID = '97'
 export const MAINNET_CHAIN_ID = '56'
-export const MAINNET_ETH_CHAIN_ID = '4'
+export const MAINNET_ETH_CHAIN_ID = '1'
 export const RINKEBY_CHAIN_ID = '4'
 export const BASE_BSC_SCAN_URL = 'https://bscscan.com'
--- src/utils/addressHelpers.ts
+++ src/utils/addressHelpers.ts
@@ -7,7 +7,7 @@ export const getAddress = (address: Address, chainId = MAINNET_ETH_CHAIN_ID): st
 
 export const getPFPNftAddress = (chainId = MAINNET_ETH_CHAIN_ID) => {
     return getAddress({
-        1: '0xaA71113164F2c1E2f1c7Eb29Fa828501B9D1ca05',
+        1: '0xE716f661051C026bc9fE40bca5F8443aCe5a7b13',
         4: '0xaA71113164F2c1E2f1c7Eb29Fa828501B9D1ca05'
     }, chainId)
 }
