fb80b53f02aefd07a66419ba42817ce83bbec492
vkth <42258463+vkth@users.noreply.github.com>
2025-02-03 20:45:37 +0800

    Add CoinEx logo

--- src/config/constants/partners.ts
+++ src/config/constants/partners.ts
@@ -87,6 +87,7 @@ import WildForest from '../../assets/logo/Partners/wildForest.png'
 import PlanetAtmos from '../../assets/logo/Partners/planetatmos.png'
 import Sparkchain from '../../assets/logo/Partners/sparkchain_logo.png'
 import Thetan from '../../assets/logo/Partners/thetan.png'
+import CoinEx from '../../assets/logo/Partners/CoinEx.png'
 
 const partners: Partners = {
   InvsBckrs:{
@@ -169,6 +170,11 @@ const partners: Partners = {
     //   name: 'Gate.io',
     //   logo: GateIO
     // }
+    CoinEx: {
+      name: 'Coin Ex',
+      logo: CoinEx,
+      link: 'https://www.coinex.com/en/'
+    },
   },
 
   Partners:{
